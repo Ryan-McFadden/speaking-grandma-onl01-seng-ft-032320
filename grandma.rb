391183b9527b068993ce0c06277164c7ea4b37e1
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-24 15:37:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase != phrase.upcase
+    return "NO, NOT SINCE 1938!"
     
   if phrase == "I LOVE YOU GRANMDA!"
     return "I LOVE YOU TOO PUMPKIN!"
